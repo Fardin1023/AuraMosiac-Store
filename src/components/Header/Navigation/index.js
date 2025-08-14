2edb5a8758582abfb398d6dfdcfbedd562dc967f
Fardin1023 <fardinkamran915@gmail.com>
2025-08-14 22:29:08 +0600

    Update index.js

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -30,12 +30,42 @@ const Navigation = () => {
               </li>
               <li className="list-inline-item">
                 <Link to="/"><Button>SkinCare-Products</Button></Link>
+                <div className="submenu shadow">
+                  <Link to="/"><Button>Cleansers</Button></Link>
+                  <Link to="/"><Button>Toners</Button></Link>
+                  <Link to="/"><Button>Moisturizers</Button></Link>
+                  <Link to="/"><Button>Serums</Button></Link>
+                  <Link to="/"><Button>Face Masks</Button></Link>
+                  <Link to="/"><Button>Sunscreens</Button></Link>
+                  <Link to="/"><Button>Eye Care</Button></Link>
+                  <Link to="/"><Button>Lip Care</Button></Link>
+                </div>
               </li>
               <li className="list-inline-item">
                 <Link to="/"><Button>Plants</Button></Link>
+                <div className="submenu shadow">
+                  <Link to="/"><Button>Indoor Plants</Button></Link>
+                  <Link to="/"><Button>Outdoor Plants</Button></Link>
+                  <Link to="/"><Button>Flowering Plants</Button></Link>
+                  <Link to="/"><Button>Succulents & Cacti</Button></Link>
+                  <Link to="/"><Button>Herbs & Edible Plants</Button></Link>
+                  <Link to="/"><Button>Air-Purifying Plants</Button></Link>
+                  <Link to="/"><Button>Bonsai & Decorative Plants</Button></Link>
+                  <Link to="/"><Button>Plant Care Accessories</Button></Link>
+                </div>
               </li>
               <li className="list-inline-item">
                 <Link to="/"><Button>Hand-Crafted Items</Button></Link>
+                <div className="submenu shadow">
+                  <Link to="/"><Button>Home Décor</Button></Link>
+                  <Link to="/"><Button>Jewelry & Accessories</Button></Link>
+                  <Link to="/"><Button>Pottery & Ceramics</Button></Link>
+                  <Link to="/"><Button>Handwoven Textiles</Button></Link>
+                  <Link to="/"><Button>Wooden Crafts</Button></Link>
+                  <Link to="/"><Button>Leather Goods</Button></Link>
+                  <Link to="/"><Button>Traditional Art & Paintings</Button></Link>
+                  <Link to="/"><Button>Gift Items & Souvenirs</Button></Link>
+                </div>
               </li>
               <li className="list-inline-item">
                 <Link to="/"><Button>Blog</Button></Link>
@@ -51,3 +81,4 @@ const Navigation = () => {
   );
 };
 export default Navigation;
+
