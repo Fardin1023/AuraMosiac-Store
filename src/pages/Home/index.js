e816be50f1080f1f12ea2b3da426b70e1ef1dc0d
Fardin1023 <fardinkamran915@gmail.com>
2025-08-15 19:35:40 +0600

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,142 @@
 import HomeBanner from "../../components/HomeBanner";
+import banner1 from "../../assets/images/banner1.png";
+import banner2 from "../../assets/images/banner2.png";
+import Button from '@mui/material/Button';
+import { FaArrowRightLong } from "react-icons/fa6";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from 'swiper/modules';
+import ProductItem from "../../components/ProductItem";
 
-const Header=()=>{
-    return(
-        <>
-        <HomeBanner/>
 
-       
-        </>
-    )
-}
+const Header = () => {
+  return (
+    <>
+      <HomeBanner />
+      <section className="homeCat">
+        <div className="container"></div>
+      </section>
+      <section className="homeProducts">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="banner">
+                <img src={banner1} alt="banner" className="cursor w-100" />
+              </div>
+              <div className="banner mt-4">
+                <img src={banner2} alt="banner" className="cursor w-100" />
+              </div>
+            </div>
+            <div className="col-md-9 productRow">
+              <div className="d-flex align-items-center">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">BEST SELLERS</h3>
+                  <p className="text-light text-sml mb-0 text-dark">
+                    Do not miss the ongoing offers until the end of Summer{" "}
+                  </p>
+                </div>
+                <Button className="viewAllBtn ml-auto">
+                  View All <FaArrowRightLong />
+                </Button>
+              </div>
+              <div className="product_row w-100 mt-4">
+                <Swiper
+                  slidesPerView={4}
+                  spaceBetween={0}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  {" "}
+                 <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                 <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+
+
+
+
+
+               <div className="d-flex align-items-center mt-5">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">NEW PRODUCTS</h3>
+                  <p className="text-light text-sml mb-0 text-dark">
+                    New products with updated stocks{" "}
+                  </p>
+                </div>
+                <Button className="viewAllBtn ml-auto">
+                  View All <FaArrowRightLong />
+                </Button>
+              </div>
+              <div className="product_row w-100 mt-4">
+                <Swiper
+                  slidesPerView={4}
+                  spaceBetween={0}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  {" "}
+                 <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                 <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                   <ProductItem/>
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 export default Header;
+
+
